fd8363d2809d23862ee7cb15dc34f67e45ddae7e
ShutovKS <schutow.kir@yandex.ru>
2023-11-29 02:03:11 +0300

    Disable rigidbody configured in objects

--- Assets/_project/Scripts/UI/SpawnOrNo/SpawnOrNoUI.cs
+++ Assets/_project/Scripts/UI/SpawnOrNo/SpawnOrNoUI.cs
@@ -13,16 +13,16 @@ public class SpawnOrNoUI : MonoBehaviour
     {
         yes.OnClick.AddListener(Yes);
         no.OnClick.AddListener(No);
-        
+
         gameObject.SetActive(false);
     }
 
     private void No()
     {
         Destroy(currentObject);
-        
+
         gameObject.SetActive(false);
-        
+
         scroolScreen.SetActive(true);
 
         scroolScreenUI.UpdateCollection();
@@ -31,36 +31,24 @@ public class SpawnOrNoUI : MonoBehaviour
     private void Yes()
     {
         currentObject.transform.SetParent(null);
-        
-        
+
         var rigidbody = currentObject.GetComponent<Rigidbody>();
+
         rigidbody.isKinematic = false;
 
-        ManipulationEvent manipulationStarted = new();
-        ManipulationEvent manipulationEnded = new();
-
-        manipulationStarted.AddListener(StartManipulation);
-        manipulationEnded.AddListener(EndManipulation);
-
-        currentObject.GetComponent<ObjectManipulator>().OnManipulationStarted = manipulationStarted;
-        currentObject.GetComponent<ObjectManipulator>().OnManipulationEnded = manipulationEnded;
+        // ManipulationEvent manipulationStarted = new();
+        // ManipulationEvent manipulationEnded = new();
+        //
+        // manipulationStarted.AddListener((_) => rigidbody.isKinematic = true);
+        // manipulationEnded.AddListener((_) => rigidbody.isKinematic = false);
+        //
+        // currentObject.GetComponent<ObjectManipulator>().OnManipulationStarted = manipulationStarted;
+        // currentObject.GetComponent<ObjectManipulator>().OnManipulationEnded = manipulationEnded;
 
         currentObject = null;
-        
+
         gameObject.SetActive(false);
-        
+
         scroolScreen.SetActive(true);
-    }
-
-    private void StartManipulation(ManipulationEventData arg0)
-    {
-        var rigidbody = currentObject.GetComponent<Rigidbody>();
-        rigidbody.isKinematic = true;
-    }
-
-    private void EndManipulation(ManipulationEventData arg0)
-    {
-        var rigidbody = currentObject.GetComponent<Rigidbody>();
-        rigidbody.isKinematic = false;
     }
 }
